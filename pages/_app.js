d06a4274fe4a0f5ee5dad7afca6bae06a7563454
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-09-25 00:12:38 +0100

    created and implemented Loading component, login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { auth } from '../firebase'
 import '../styles/globals.css'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import Login from './login'
+import Loading from '../components/Loading'
 
 function MyApp({ Component, pageProps }) {
+  const [user, loading] = useAuthState(auth)
+
+  if(loading) return <Loading />
+  if(!user) return <Login />
   return <Component {...pageProps} />
 }
 
---
+++ pages/login.js
@@ -0,0 +1,48 @@
+import { Button } from "@material-ui/core"
+import Head from "next/head"
+import styled from "styled-components"
+import { signInWithGoogle } from "../firebase"
+
+function Login(){
+    const signIn = async () => {
+        let res = await signInWithGoogle();
+        console.log(res);
+    }
+
+    return (
+        <Container>
+            <Head>
+                <title>Login</title>
+            </Head>
+
+            <LoginContainer>
+                <Logo src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"/>
+                <Button variant="outlined" onClick={signIn}>Sign In With Google</Button>
+            </LoginContainer>
+        </Container>
+    )
+}
+export default Login
+
+const Container = styled.div`
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    background: whitesmoke;
+`
+
+const LoginContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 100px;
+    align-items: center;
+    background: white;
+    box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
+    border-radius: 5px;
+`
+
+const Logo = styled.img`
+    height: 200px;
+    width: 200px;
+    margin-bottom: 50px;
+`
